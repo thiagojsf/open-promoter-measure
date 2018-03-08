c3e3c004e081322f5132255d8242a4e63beb2428
pongelupe <pongelupe1998@gmail.com>
2018-03-08 13:21:14 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
