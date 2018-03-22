c180b18cd899aa5201765ec1f4cff10856f767cb
Thiago Jordan de Souza Ferreira <thiago.desouza@linx.com.br>
2018-03-22 13:08:25 -0300

    Adição do código da filial no filtro dos títulos

--- src/angularjs/titulos/titulo.controller.js
+++ src/angularjs/titulos/titulo.controller.js
@@ -12,50 +12,50 @@ modulo.controller("tituloController", function($scope, svcTitulos, svcFilial) {
    $scope.fornecedor = "SAMSUNG";
    $scope.filial = "Linx SP";
 
-   svcTitulos.carregaTitulosAPagar(function(res){
-      $scope.Titulos = res.data;
-      for (var i = 0; i < $scope.Titulos.length; i++) {
+   $scope.carregarTitulosAPagar = function(){
+      svcTitulos.carregaTitulosAPagar(function(res){
+         $scope.Titulos = res.data;
+         for (var i = 0; i < $scope.Titulos.length; i++) {
 
-         $scope.Titulos[i].Emissao = new Date($scope.Titulos[i].Emissao);
-         $scope.Titulos[i].Vencimento = new Date($scope.Titulos[i].Vencimento);
+            $scope.Titulos[i].Emissao = new Date($scope.Titulos[i].Emissao);
+            $scope.Titulos[i].Vencimento = new Date($scope.Titulos[i].Vencimento);
 
-         if ($scope.Titulos[i].Vencimento <= new Date())
-         {
-            $scope.Titulos[i].TextoVencimento = "Venceu";
-            $scope.Titulos[i].EstaVencido = true;
+            if ($scope.Titulos[i].Vencimento <= new Date())
+            {
+               $scope.Titulos[i].TextoVencimento = "Venceu";
+               $scope.Titulos[i].EstaVencido = true;
+               
+            }
+            else
+            {
+               $scope.Titulos[i].TextoVencimento = "Vence";
+               $scope.Titulos[i].EstaVencido = false;
+            }
+
+            if ($scope.Titulos[i].LxStatusDocName != undefined && $scope.Titulos[i].LxStatusDocName.length > 0)
+            {
+               $scope.Titulos[i].AbreviacaoTitulo = $scope.Titulos[i].LxStatusDocName.substring(0,1);
+            }
+            else
+            {
+
+            }
             
          }
-         else
-         {
-            $scope.Titulos[i].TextoVencimento = "Vence";
-            $scope.Titulos[i].EstaVencido = false;
-         }
-
-         if ($scope.Titulos[i].LxStatusDocName != undefined && $scope.Titulos[i].LxStatusDocName.length > 0)
-         {
-            $scope.Titulos[i].AbreviacaoTitulo = $scope.Titulos[i].LxStatusDocName.substring(0,1);
-         }
-         else
-         {
-
-         }
-         
-      }
-   });
-
-   setTimeout(function(){
-      console.log("X");
-   }, 20000);
+      }, $scope.FilialSelecionada == undefined ? null : $scope.FilialSelecionada.CodigoFilial);
+   }
 
    svcFilial.carregarFiliais(function(res){
       $scope.Filiais = res.data.Results;
       $scope.FilialSelecionada = $scope.Filiais[0];
       $scope.ExibirMatrizesContabeis = true;
       $scope.ExibirFiliais = true;
+      $scope.carregarTitulosAPagar();
    }, 500);
    
    $scope.selecionarFilial = function(filial) {
       $scope.FilialSelecionada = filial;
+      $scope.carregarTitulosAPagar();
    }
    
    
--- src/angularjs/titulos/titulo.factory.js
+++ src/angularjs/titulos/titulo.factory.js
@@ -1,9 +1,15 @@
 modulo.factory("svcTitulos", function($http){
    return {
-      carregaTitulosAPagar : function(emCasoDeSucesso){
+      carregaTitulosAPagar : function(emCasoDeSucesso, codigoFilial){
+
+         var parametros = {
+            "$orderby" : "Adquirente",
+            "jEntitySearch" : "FinReceberAnalitico{EmissaoDe#>=#T2018-03-01%2000%3A00%3A00.000;EmissaoDe#<=#T2018-03-01%2023%3A59%3A59.999;LxTipo#==#I1;CodigoFilial#==#S"+codigoFilial+"}"
+         };
+
          request(urlTitulos, $http, function(res){
             emCasoDeSucesso(res);
-         })
+         }, parametros);
       }
    }
 });
