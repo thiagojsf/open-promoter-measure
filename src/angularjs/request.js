afe3d7c68e52c923688ac85e02565734abcab987
Thiago Jordan de Souza Ferreira <thiago.desouza@linx.com.br>
2018-03-22 14:05:41 -0300

    Inclusão do código da filial na factorty

--- src/angularjs/request.js
+++ src/angularjs/request.js
@@ -2,7 +2,7 @@ var request = function(url, $http, emCasoDeSucesso, parametros){
 		
    var $headers = {
          Application: "6d26d800-bbf8-4843-bca9-51dbfa200bc2",
-         AuthorizationToken: "b855857d-4a05-4cdf-aefb-50e861e0914a",
+         AuthorizationToken: "5f3704a7-c7ac-421d-8a01-5893bba88e8f",
          Branch: -1,
          CurrentCompany: "74351065-0a60-48b7-9774-ba97d99f1c9f",
          CurrentUser: "77d21861-51fa-4af1-8063-3392c65936ac",
--- src/angularjs/titulos/titulo.factory.js
+++ src/angularjs/titulos/titulo.factory.js
@@ -4,7 +4,7 @@ modulo.factory("svcTitulos", function($http){
 
          var parametros = {
             "$orderby" : "Adquirente",
-            "jEntitySearch" : "FinReceberAnalitico{EmissaoDe#>=#T2018-03-01%2000%3A00%3A00.000;EmissaoDe#<=#T2018-03-01%2023%3A59%3A59.999;LxTipo#==#I1;CodigoFilial#==#S"+codigoFilial+"}"
+            "jEntitySearch" : "FinReceberAnalitico{CodigoFilial#==#S"+codigoFilial+"}"
          };
 
          request(urlTitulos, $http, function(res){
